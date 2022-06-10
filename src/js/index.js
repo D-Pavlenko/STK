12a15f621e18474b9452204dbe31a92643967c3c
D-Pavlenko <denispavlenko94@gmail.com>
2022-06-10 10:25:04 +0300

    STK_v3.0

--- src/js/index.js
+++ src/js/index.js
@@ -33,13 +33,137 @@ function toggleMobileNav() {
     body.classList.toggle('no-scroll');
 }
 
+// Swiper Doctors
+
+const swiperDoctors = new Swiper('.doctors-swiper', {
+    spaceBetween: 20,
+
+    loop: true,
+    speed: 600,
+
+    slidesPerView: 4,
+
+
+    breakpoints: {
+        1330: {
+            slidesPerView: 4,
+        },
+
+        1200: {
+            slidesPerView: 3.45,
+        },
+
+        1150: {
+            slidesPerView: 3.25,
+        },
+
+        992: {
+            slidesPerView: 2.8,
+        },
+
+        910: {
+            slidesPerView: 2.7,
+        },
+
+        840: {
+            slidesPerView: 2.5,
+        },
+
+
+        790: {
+            slidesPerView: 2.3,
+        },
+
+        750: {
+            slidesPerView: 2.2,
+        },
+
+        670: {
+            slidesPerView: 2,
+        },
+
+        630: {
+            slidesPerView: 1.85,
+        },
+
+        580: {
+            slidesPerView: 1.65,
+        },
+
+        550: {
+            slidesPerView: 1.55,
+        },
+
+        520: {
+            slidesPerView: 1.35,
+        },
+
+        490: {
+            slidesPerView: 1.45,
+        },
+
+        460: {
+            slidesPerView: 1.35,
+            spaceBetween: 15,
+        },
+
+        430: {
+            slidesPerView: 1.25,
+        },
+
+        375: {
+            slidesPerView: 1.25,
+        },
+
+        360: {
+            slidesPerView: 1.1,
+            spaceBetween: 10,
+        },
+
+        340: {
+            slidesPerView: 1.1,
+            spaceBetween: 10,
+        },
+
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+    },
+
+    navigation: {
+        prevEl: '.doctors__button-prev',
+        nextEl: '.doctors__button-next'
+    },
+
+    pagination: {
+        el: '.doctors__swiper-pagination',
+        clickable: true,
+    },
+
+    mousewheel: {
+        invert: false
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+});
+
 // Swiper Diplomas
 
 const swiperDiplomas = new Swiper('.diplomas-swiper', {
     spaceBetween: 20,
 
     loop: true,
-    speed: 1400,
+    speed: 600,
 
     slidesPerView: 6,
 
@@ -110,7 +234,7 @@ const swiperTools = new Swiper('.tools-swiper', {
     spaceBetween: 20,
 
     loop: true,
-    speed: 1400,
+    speed: 600,
 
     pagination: {
         el: '.tools-swiper-pagination',
@@ -132,3 +256,185 @@ const swiperTools = new Swiper('.tools-swiper', {
         pageUpDown: true,
     },
 });
+
+// Swiper Feedback
+
+const swiperFeedback = new Swiper('.feedback-swiper', {
+    spaceBetween: 20,
+
+    loop: true,
+    speed: 600,
+
+    slidesPerView: 2,
+
+    breakpoints: {
+        661: {
+            slidesPerView: 2,
+        },
+
+        660: {
+            slidesPerView: 1,
+        },
+
+        280: {
+            slidesPerView: 1,
+        },
+    },
+
+    pagination: {
+        el: '.feedback-swiper-pagination ',
+        clickable: true,
+    },
+
+    navigation: {
+        prevEl: '.feedback-button-prev',
+        nextEl: '.feedback-button-next',
+    },
+
+    mousewheel: {
+        invert: false
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+});
+
+
+// Read More
+
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".feedback-slide__text");
+
+readMoreBtn.addEventListener("click", (e) => {
+    text.classList.toggle("show-more");
+    if (readMoreBtn.innerText === "Читать полностью") {
+        readMoreBtn.innerText = "Свернуть";
+    } else {
+        readMoreBtn.innerText = "Читать полностью";
+    }
+});
+
+// Swiper Video Feedback
+
+const swiperVideoFeedback = new Swiper('.video__feedback-swiper', {
+    spaceBetween: 20,
+
+    loop: true,
+    speed: 600,
+
+    slidesPerView: 2,
+
+    breakpoints: {
+        661: {
+            slidesPerView: 2,
+        },
+
+        660: {
+            slidesPerView: 1,
+        },
+
+        280: {
+            slidesPerView: 1,
+        },
+    },
+
+    pagination: {
+        el: '.video__feedback-swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        prevEl: '.video__feedback-button-prev',
+        nextEl: '.video__feedback-button-next',
+    },
+
+    mousewheel: {
+        invert: false
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+});
+
+// Swiper Photogallery
+
+const swiperPhotogallery = new Swiper('.photogallery-swiper', {
+    spaceBetween: 0,
+
+    loop: true,
+    speed: 600,
+
+    slidesPerView: 1,
+
+    navigation: {
+        prevEl: '.photogallery__button-prev',
+        nextEl: '.photogallery__button-next'
+    },
+
+    pagination: {
+        el: '.photogallery__swiper-pagination',
+        clickable: true,
+    },
+
+    mousewheel: {
+        invert: false
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+});
+
+document.querySelectorAll('.accordion_item__trigger').forEach((item) =>
+    item.addEventListener('click', () => {
+        const parent = item.parentNode;
+
+        if (parent.classList.contains('accordion_item__active')) {
+            parent.classList.remove('accordion_item__active')
+        } else {
+            document
+                .querySelectorAll('.accordion_item')
+                .forEach((child) => child.classList.remove('accordion_item__active'))
+            parent.classList.add('accordion_item__active')
+        }
+    })
+)
+
+// Map
+
+let center = [53.19416657445425, 50.14532429095446]
+
+function init() {
+    let map = new ymaps.Map('map', {
+        center: center,
+        zoom: 16,
+    })
+
+    let placemark = new ymaps.Placemark(center, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/contacts/geo.png',
+        iconImageSize: [47, 58],
+        iconImageOffset: [300, -150],
+    })
+
+    map.controls.remove('geolocationControl');
+    map.controls.remove('searchControl');
+    map.controls.remove('trafficControl');
+    map.controls.remove('typeSelector');
+    map.controls.remove('fullscreenControl');
+    map.controls.remove('zoomControl');
+    map.controls.remove('rulerControl');
+    map.behaviors.disable(['scrollZoom']);
+
+    map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
